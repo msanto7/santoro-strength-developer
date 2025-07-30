cfb1ef9e9ad38f5ca98e31bb24bb1667b1986b3c
msanto7 <mike.santoro42@gmail.com>
2025-07-30 00:18:01 -0400

    working dev conatiner - db creation

--- StrengthPortal/database-scripts/01-create-tables.sql
+++ StrengthPortal/database-scripts/01-create-tables.sql
@@ -4,18 +4,24 @@
 USE StrengthPortal;
 GO
 
--- Create User table
-CREATE TABLE [dbo].[User] (
-    [UserID] UNIQUEIDENTIFIER NOT NULL,
-    [Email]  NVARCHAR(255) NULL,
-    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserID] ASC)
-);
+-- Create User table if it doesn't exist
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='User' AND xtype='U')
+BEGIN
+    CREATE TABLE [dbo].[User] (
+        [UserID] UNIQUEIDENTIFIER NOT NULL,
+        [Email]  NVARCHAR(255) NULL,
+        CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserID] ASC)
+    );
+END
 GO
 
--- Insert sample data for development
-INSERT INTO [dbo].[User] (UserID, Email) VALUES 
-    (NEWID(), 'admin@strengthportal.com'),
-    (NEWID(), 'user@strengthportal.com');
+-- Insert sample data for development (only if table is empty)
+IF NOT EXISTS (SELECT 1 FROM [dbo].[User])
+BEGIN
+    INSERT INTO [dbo].[User] (UserID, Email) VALUES 
+        (NEWID(), 'admin@strengthportal.com'),
+        (NEWID(), 'user@strengthportal.com');
+END
 GO
 
 PRINT 'Database tables created successfully';
